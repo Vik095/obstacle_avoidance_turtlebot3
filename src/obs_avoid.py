05ca11c5588f52374bbc1acb94391ec357c2c591
Vik095 <vikhaash.kanagavel@gmail.com>
2024-06-21 13:56:26 -0400

    Update

--- src/obs_avoid.py
+++ src/obs_avoid.py
@@ -1,69 +1,63 @@
 #!/usr/bin/env python3
-import rospy # Python library for ROS
-from sensor_msgs.msg import LaserScan # LaserScan type message is defined in sensor_msgs
-from geometry_msgs.msg import Twist #
+import rospy  # Python library for ROS
+from sensor_msgs.msg import LaserScan  # LaserScan type message is defined in sensor_msgs
+from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
-
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import Point, Quaternion
 import numpy as np
 
 
-curr_pos = Point()
+class Nav:
+    def __init__(self):
+        self.curr_pos = Point()
+        self.target = Point()
+        self.orientation = Quaternion()
+
+        self.target.x = 1
+        self.target.y = 0
+        self.target.z = 0
+
+        self.move = Twist()
+        self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+        rospy.Subscriber("/scan", LaserScan, self.callback_scan)
+        rospy.Subscriber("/odom", Odometry, self.callback_odom)
+
+    def controlUGV(self, u_lin, u_ang):
+        twist = Twist()
+        twist.linear.x = u_lin
+        twist.angular.z = u_ang
+        self.pub.publish(twist)
+
+    def callback_scan(self, dt):
+        print('Range data at 0 deg:   {}'.format(dt.ranges[0]))
+        print('Range data at 15 deg:  {}'.format(dt.ranges[15]))
+        print('Range data at 345 deg: {}'.format(dt.ranges[345]))
+
+        thr1 = 0.8  # Laser scan range threshold
+        thr2 = 0.8
+        if dt.ranges[0] > thr1 and dt.ranges[15] > thr2 and dt.ranges[30] > thr2:
+            self.move.linear.x = 0.5
+            self.move.angular.z = 0.0
+        else:
+            self.move.linear.x = 0.0  # stop
+            self.move.angular.z = 0.5
+            if dt.ranges[0] > thr1 and dt.ranges[15] > thr2 and dt.ranges[345] > thr2:
+                self.move.linear.x = 0.5
+                self.move.angular.z = 0.0
+
+        self.pub.publish(self.move)
+
+    def callback_odom(self, dt):
+        self.curr_pos.x = dt.pose.pose.position.x
+        self.curr_pos.y = dt.pose.pose.position.y
+        self.curr_pos.z = dt.pose.pose.position.z
+        if (self.target.x - self.curr_pos.x > 0.2):
+            self.controlUGV(1, 0)
+        else:
+            self.controlUGV(0, 0)
 
 
-target = Point()
-
-target.x= 1
-target.y=0
-target.z=0
-
-
-def controlUGV(u_lin, u_ang):
-    twist =Twist()
-    twist.linear.x = u_lin
-    twist.angular.z=u_ang
-    pub.publish(twist)
-
-
-
-def callback_scan(dt):
-    
-    print ('Range data at 0 deg:   {}'.format(dt.ranges[0]))
-    print ('Range data at 15 deg:  {}'.format(dt.ranges[15]))
-    print ('Range data at 345 deg: {}'.format(dt.ranges[345]))
-    min_angle = dt.angle_min
-    max_angle = dt.angle_max
-    step = dt.angle_increment
-
-    thr1 = 0.8 # Laser scan range threshold
-    thr2 = 0.8
-    if dt.ranges[0]>thr1 and dt.ranges[15]>thr2 and dt.ranges[345]>thr2: 
-        move.linear.x = 0.5 
-        move.angular.z = 0.0
-    else:
-        move.linear.x = 0.0 # stop
-        move.angular.z = 0.5
-        if dt.ranges[0]>thr1 and dt.ranges[15]>thr2 and dt.ranges[345]>thr2:
-            move.linear.x = 0.5
-            move.angular.z = 0.0
-    pub.publish(move) 
-
-def callback_odom(dt):
-    curr_pos.x = dt.pose.pose.position.x
-    curr_pos.y= dt.pose.pose.position.y
-    curr_pos.z = dt.pose.pose.position.z
-    if(target.x-curr_pos.x>0.2):
-        controlUGV(1,0)
-    else:
-        controlUGV(0,0)
-
-
-move = Twist() 
-rospy.init_node('obstacle_avoidance_node') 
-pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10) 
-#sub = rospy.Subscriber("/scan", LaserScan, callback_scan) 
-pos_sub = rospy.Subscriber("/odom", Odometry, callback_odom)
-
-
-rospy.spin() 
-
+if __name__ == '__main__':
+    rospy.init_node('obstacle_avoidance_node')
+    nav = Nav()
+    rospy.spin()
